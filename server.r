55596af404ecfcec6032d84704a048dca4415f91
bendenis <bdshaff@umich.edu>
2017-07-10 11:58:48 -0400

    Financial page, some UI changes, and continuous reactivity.

--- server.r
+++ server.r
@@ -6,23 +6,32 @@ library(data.table)
 
 shinyServer(function(input,output) {
         
+        #### PAGE 1
+        
         dt1 = reactive({
                 tb = DT %>% filter(Region == input$region, state == input$state,
                               four_year == input$four_year | two_year == input$two_year,
                               public_or_private == input$private | public_or_private != input$public,
                               ugrad_total_pop <= input$ugrad_total_pop, 
-                              coed == input$coed,
-                              religious_affiliation == input$religious,
-                              specialized == input$specialized) %>% 
+                              coed == input$coed) %>% 
                         arrange(uni_ranking) %>% as.data.frame()
+                
+                if(input$religious == 1){
+                        tb = tb[tb$religious_affiliation == 1,]
+                }
+                
+                if(input$specialized == 1){
+                        tb = tb[tb$specialized == 1,]
+                }
+                
                 #tb[sapply(tb$subjects_offered, function(x) str_detect(x, input$major_selecotr)), ]
                 tb
         })
         
-        output$school_size = renderDataTable({
-                dt1() %>% select(school_name, city, ugrad_total_pop, uni_ranking)
+        output$list1 = renderDataTable({
+                dt1() %>% select(school_name, city, uni_ranking)
         },
-        options = list(pageLength = 10))
+        options = list(pageLength = 20))
         
         output$college_map = renderLeaflet({
                 
@@ -41,8 +50,11 @@ shinyServer(function(input,output) {
                                          popup = popup)
         })
         
+        
+        #### PAGE 2 ACADEMIC
+        
         dt2 = reactive({
-                DT %>% filter(SAT_avg_composite <= input$SAT_avg_composite_up,
+                dt1() %>% filter(SAT_avg_composite <= input$SAT_avg_composite_up,
                               SAT_avg_composite >= input$SAT_avg_composite_lw,
                               SAT_avg_math <= input$SAT_avg_math_up, 
                               SAT_avg_math >= input$SAT_avg_math_lw,
@@ -58,17 +70,105 @@ shinyServer(function(input,output) {
                               ACT_avg_eng >= input$ACT_avg_eng_lw,
                               ACT_avg_writing <= input$ACT_avg_writing_up,
                               ACT_avg_writing >= input$ACT_avg_writing_lw) %>% 
-                        arrange(uni_ranking) %>%
-                        select(school_name, state, city,
+                        arrange(uni_ranking)
+        })
+
+        output$list2 = renderDataTable({
+                dt2() %>% select(school_name, state, city,
                                SAT_range_upper, SAT_range_lower, 
                                ACT_range_upper, ACT_range_lower)
+        },
+        options = list(pageLength = 20))
+        
+        output$college_map2 = renderLeaflet({
+                
+                popup = str_c(sep = "<br/>",
+                              "<b><a href='http://www.myklovr.com'> MyKlovr </a></b>",
+                              "1350 6th Ave.",
+                              "New York, NY 98138")
+                
+                leaflet(data = dt2()) %>% 
+                        setView(lng = mean(dt2()$Longitude, na.rm = T), lat = mean(dt2()$Latitude, na.rm = T), zoom = 7) %>% 
+                        addTiles() %>%
+                        addCircleMarkers(lng = ~Longitude, 
+                                         lat = ~Latitude, 
+                                         label = ~as.character(dt2()$school_name),
+                                         clusterOptions = markerClusterOptions(zIndexOffset = 5),
+                                         popup = popup)
         })
         
+        output$gpa_density = renderPlot({
+                gpa_dt = DT %>% select(school_name,
+                                       GPA_3.75_higher, GPA_3.50_3.74, 
+                                       GPA_3.25_3.49, GPA_3_3.24, GPA_3_3.24,
+                                       GPA_2.50_2.99, GPA_2.0_2.49, GPA_1.0_1.99, GPA_below_1)
+                sp = c(3.875,3.625,3.37,3.12, 2.745, 2.25, 1.495, 0.5)
+                pr = as.numeric(gpa_dt[school_name == input$school_name_gpa,-1]/100)
+                dn = sample(sp, size = 10000, prob = pr, replace = T)
+                g = ggplot(data.frame(dn), aes(x = dn)) + 
+                        stat_density(bw = 0.2, alpha = 0.5) + 
+                        xlim(2,4)
+                g
+        })
 
-        output$college_list = renderDataTable({
-                dt2()
+        #### PAGE 3
+        
+        dt3 = reactive({
+                tb = dt2() %>% filter(tuition_instate <= input$tuition)
+                
+                if(input$nb_scholarship == 1){
+                        tb = tb[tb$nb_scholarship == 1,]
+                }
+                if(input$SEOG_scholarship == 1){
+                        tb = tb[tb$SEOG_scholarship == 1,]
+                }
+                if(input$state_scholarship == 1){
+                        tb = tb[tb$state_scholarship == 1,]
+                }
+                if(input$college_scholarship == 1){
+                        tb = tb[tb$college_scholarship == 1,]
+                }
+                if(input$private_scholarship == 1){
+                        tb = tb[tb$private_scholarship == 1,]
+                }
+                if(input$nursing_scholarship == 1){
+                        tb = tb[tb$SEOG_scholarship == 1,]
+                }
+                if(input$nursing_scholarship == 1){
+                        tb = tb[tb$SEOG_scholarship == 1,]
+                }
+                if(input$united_negro_scholarship == 1){
+                        tb = tb[tb$united_negro_scholarship == 1,]
+                }
+                tb
+                
         })
         
+        output$list3 = renderDataTable({
+                dt3() %>% select(school_name, city, uni_ranking, tuition_instate, tuition_outstate)
+                
+        },
+        options = list(pageLength = 20))
+        
+        output$college_map3 = renderLeaflet({
+                
+                popup = str_c(sep = "<br/>",
+                              "<b><a href='http://www.myklovr.com'> MyKlovr </a></b>",
+                              "1350 6th Ave.",
+                              "New York, NY 98138")
+                
+                leaflet(data = dt3()) %>% 
+                        setView(lng = mean(dt3()$Longitude, na.rm = T), lat = mean(dt3()$Latitude, na.rm = T), zoom = 7) %>% 
+                        addTiles() %>%
+                        addCircleMarkers(lng = ~Longitude, 
+                                         lat = ~Latitude, 
+                                         label = ~as.character(dt3()$school_name),
+                                         clusterOptions = markerClusterOptions(zIndexOffset = 5),
+                                         popup = popup)
+        })
+        
+        #### PAGE 4
+                
         athletic_school_name = reactive({input$athletic_school_name})
 
         output$men_club_sports = renderTable({
@@ -94,20 +194,6 @@ shinyServer(function(input,output) {
         output$alumni_list = renderTable({
                 DT %>% filter(school_name == athletic_school_name()) %>%
                         select(alum_list)
-        })
-        
-        output$gpa_density = renderPlot({
-                gpa_dt = DT %>% select(school_name,
-                                       GPA_3.75_higher, GPA_3.50_3.74, 
-                                       GPA_3.25_3.49, GPA_3_3.24, GPA_3_3.24,
-                                       GPA_2.50_2.99, GPA_2.0_2.49, GPA_1.0_1.99, GPA_below_1)
-                sp = c(3.875,3.625,3.37,3.12, 2.745, 2.25, 1.495, 0.5)
-                pr = as.numeric(gpa_dt[school_name == input$school_name_gpa,-1]/100)
-                dn = sample(sp, size = 10000, prob = pr, replace = T)
-                g = ggplot(data.frame(dn), aes(x = dn)) + 
-                        stat_density(bw = 0.2, alpha = 0.5) + 
-                        xlim(2,4)
-                g
         })
         
         output$diversity_plot = renderPlotly({
--- ui.r
+++ ui.r
@@ -6,88 +6,128 @@ library(ggplot2)
 library(plotly)
 library(data.table)
 library(stringr)
-DT = fread("Data/collegeFinderDataInjected.csv")
+DT = fread("/Users/bendenisshaffer/Dropbox/myKlovr/CollegeFinder/Data/Interim/collegeFinderDataInjected.csv")
 
 
 shinyUI(navbarPage(title = "College Finder App",theme = shinytheme("united"),
         
         tabPanel("School Type",
                  sidebarPanel(
+                         h1("Location"),
                          selectInput(inputId = "region", label = "Region",
                                      choices = unique(DT$Region), selected = "N"),
                          selectInput(inputId = "state", label = "Chose State", 
-                                     choices = unique(DT$state), multiple = T, selected = c("MA","NY")),
+                                     choices = unique(DT$state), multiple = T, selected = c("NY")),
+                         h1("School Type"),
                          sliderInput(inputId = "ugrad_total_pop", label = "School Size", 
                                      min = 0, max = 70000, step = 20, value = 40000),
                          checkboxInput(inputId = "four_year", label = "4 Year"),
                          checkboxInput(inputId = "two_year", label = "2 Year"),
                          checkboxInput(inputId = "private", label = "Private"),
                          checkboxInput(inputId = "public", label = "Public"),
+                         checkboxInput(inputId = "religious", label = "Has Religious Affiliation"),
+                         checkboxInput(inputId = "specialized", label = "Specialized School"),
                          radioButtons(inputId = "coed", label = "Co-Educational:",
                                       choices = c("COED" = 3,
                                                   "All Female" = 1,
-                                                  "All Male" = 2)),
-                         checkboxInput(inputId = "religious", label = "Has Religious Affiliation"),
-                         checkboxInput(inputId = "specialized", label = "Specialized School"),
-                         selectInput(inputId = "major_selecotr", label = "Choose Majors", 
-                                     choices = str_trim(unique(unlist(str_split(DT$subjects_offered, pattern = ","))))[-1],
-                                     multiple = T, selected = "Mathematics & Statistics")
+                                                  "All Male" = 2))
                  ),
+                 
                  mainPanel(
                          leafletOutput("college_map"),
-                         dataTableOutput("school_size")
+                         dataTableOutput("list1")
                  )
                  
                  ),
         
         navbarMenu(title = "Academic",   
-                   tabPanel("Standardized Tests",
+                   tabPanel("Majors",
                             sidebarPanel(
-                                    sliderInput(inputId = "SAT_avg_composite_up", label = "Average SAT Upper Bound", 
-                                                min = 0, max = 1600, value = 1500, step = 10),
-                                    sliderInput(inputId = "SAT_avg_composite_lw", label = "Average SAT Lower Bound", 
-                                                min = 0, max = 1600, value = 400, step = 10),
-                                    sliderInput(inputId = "SAT_avg_math_up", label = "Average SAT MATH Upper Bound", 
-                                                min = 0, max = 800, value = 700, step = 10),
-                                    sliderInput(inputId = "SAT_avg_math_lw", label = "Average SAT MATH Lower Bound", 
-                                                min = 0, max = 800, value = 500, step = 10),
-                                    sliderInput(inputId = "SAT_avg_reading_up", label = "Average SAT READING Upper Bound", 
-                                                min = 0, max = 800, value = 700, step = 10),
-                                    sliderInput(inputId = "SAT_avg_reading_lw", label = "Average SAT READING Lower Bound", 
-                                                min = 0, max = 800, value = 500, step = 10),
-                                    sliderInput(inputId = "SAT_avg_writing_up", label = "Average SAT WRITING Upper Bound", 
-                                                min = 0, max = 2200, value = 2100, step = 10),
-                                    sliderInput(inputId = "SAT_avg_writing_lw", label = "Average SAT WRITING Lower Bound", 
-                                                min = 0, max = 2200, value = 400, step = 10),
-                                    sliderInput(inputId = "ACT_avg_composite_up", label = "Average ACT Upper Bound",
-                                                min = 0, max = 36, value = 30, step = 1),
-                                    sliderInput(inputId = "ACT_avg_composite_lw", label = "Average ACT Lower Bound",
-                                                min = 0, max = 36, value = 0, step = 1),
-                                    sliderInput(inputId = "ACT_avg_math_up", label = "Average ACT MATH Upper Bound",
-                                                min = 0, max = 36, value = 30, step = 1),
-                                    sliderInput(inputId = "ACT_avg_math_lw", label = "Average ACT MATH Lower Bound",
-                                                min = 0, max = 36, value = 0, step = 1),
-                                    sliderInput(inputId = "ACT_avg_eng_up", label = "Average ACT ENGLISH Upper Bound",
-                                                min = 0, max = 36, value = 30, step = 1),
-                                    sliderInput(inputId = "ACT_avg_eng_lw", label = "Average ACT ENGLISH Lower Bound",
-                                                min = 0, max = 36, value = 0, step = 1),
-                                    sliderInput(inputId = "ACT_avg_writing_up", label = "Average ACT WRITING Upper Bound",
-                                                min = 0, max = 36, value = 30, step = 1),
-                                    sliderInput(inputId = "ACT_avg_writing_lw", label = "Average ACT WRITING Lower Bound",
-                                                min = 0, max = 36, value = 0, step = 1)
+                                    h1("Majors"),
+                                    p("Select multiple majors that you are interested in."),
+                                    selectInput(inputId = "major_selecotr", label = "Choose Majors", 
+                                                choices = str_trim(unique(unlist(str_split(DT$subjects_offered, pattern = ","))))[-1],
+                                                multiple = T, selected = "Mathematics & Statistics")
                             ),
                             
                             mainPanel(
-                                    dataTableOutput("college_list")
+                                    leafletOutput("college_map2"),
+                                    dataTableOutput("list2")
                             )
                    ),
                    
-                   tabPanel("GPA Distributions",
-                            selectInput(inputId = "school_name_gpa", label = "Type School Name", choices = unique(DT$school_name),
-                                        selected = "University of Michigan -- Ann Arbor"),
-                            plotOutput("gpa_density"))
+                   tabPanel("SAT",
+                            sidebarPanel(
+                                    sliderInput(inputId = "SAT_avg_composite_up", label = "Average SAT Upper Bound", 
+                                                min = 0, max = 1600, value = 1600, step = 10),
+                                    sliderInput(inputId = "SAT_avg_composite_lw", label = "Average SAT Lower Bound", 
+                                                min = 0, max = 1600, value = 0, step = 10),
+                                    sliderInput(inputId = "SAT_avg_math_up", label = "Average SAT MATH Upper Bound", 
+                                                min = 0, max = 800, value = 800, step = 10),
+                                    sliderInput(inputId = "SAT_avg_math_lw", label = "Average SAT MATH Lower Bound", 
+                                                min = 0, max = 800, value = 0, step = 10),
+                                    sliderInput(inputId = "SAT_avg_reading_up", label = "Average SAT READING Upper Bound", 
+                                                min = 0, max = 800, value = 800, step = 10),
+                                    sliderInput(inputId = "SAT_avg_reading_lw", label = "Average SAT READING Lower Bound", 
+                                                min = 0, max = 800, value = 0, step = 10),
+                                    sliderInput(inputId = "SAT_avg_writing_up", label = "Average SAT WRITING Upper Bound", 
+                                                min = 0, max = 2200, value = 2200, step = 10),
+                                    sliderInput(inputId = "SAT_avg_writing_lw", label = "Average SAT WRITING Lower Bound", 
+                                                min = 0, max = 2200, value = 0, step = 10)
+                                    )
+                            ),
+                   
+                   tabPanel("ACT",
+                            sidebarPanel(
+                                    sliderInput(inputId = "ACT_avg_composite_up", label = "Average ACT Upper Bound",
+                                                min = 0, max = 36, value = 36, step = 1),
+                                    sliderInput(inputId = "ACT_avg_composite_lw", label = "Average ACT Lower Bound",
+                                                min = 0, max = 36, value = 0, step = 1),
+                                    sliderInput(inputId = "ACT_avg_math_up", label = "Average ACT MATH Upper Bound",
+                                                min = 0, max = 36, value = 36, step = 1),
+                                    sliderInput(inputId = "ACT_avg_math_lw", label = "Average ACT MATH Lower Bound",
+                                                min = 0, max = 36, value = 0, step = 1),
+                                    sliderInput(inputId = "ACT_avg_eng_up", label = "Average ACT ENGLISH Upper Bound",
+                                                min = 0, max = 36, value = 36, step = 1),
+                                    sliderInput(inputId = "ACT_avg_eng_lw", label = "Average ACT ENGLISH Lower Bound",
+                                                min = 0, max = 36, value = 0, step = 1),
+                                    sliderInput(inputId = "ACT_avg_writing_up", label = "Average ACT WRITING Upper Bound",
+                                                min = 0, max = 36, value = 36, step = 1),
+                                    sliderInput(inputId = "ACT_avg_writing_lw", label = "Average ACT WRITING Lower Bound",
+                                                min = 0, max = 36, value = 0, step = 1)
+                                    )
+                            )#,
+                   
+                   
+                   # tabPanel("GPA Distributions",
+                   #          selectInput(inputId = "school_name_gpa", label = "Type School Name", choices = unique(DT$school_name),
+                   #                      selected = "University of Michigan -- Ann Arbor"),
+                   #          plotOutput("gpa_density"))
                    
         ),
+        
+        tabPanel(title = "Financial",
+                 sidebarPanel(
+                        h1("Tuition"),
+                        sliderInput(inputId = "tuition", label = "Maximum Tuition",
+                             min = 0, max = 100000, value = 100000),
+                        h1("Scholarships"),
+                        checkboxInput(inputId = "nb_scholarship", label = "Need Based Scholarships"),
+                        checkboxInput(inputId = "pell_grant_scholarship", label = "Pell Grant"),
+                        checkboxInput(inputId = "SEOG_scholarship", label = "SEOG"),
+                        checkboxInput(inputId = "state_scholarship", label = "State Scholarships"),
+                        checkboxInput(inputId = "college_scholarship", label = "College Scholarships"),
+                        checkboxInput(inputId = "private_scholarship", label = "Private Scholarships"),
+                        checkboxInput(inputId = "nursing_scholarship", label = "Nursing Scholarships"),
+                        checkboxInput(inputId = "united_negro_scholarship", label = "United Negro Scholarships")
+                        ),
+                 
+                        mainPanel(
+                                leafletOutput("college_map3"),
+                                dataTableOutput("list3")
+                        )
+                 
+                 ),
 
         tabPanel(title = "University Life",
                 selectInput(inputId = "athletic_school_name", 
