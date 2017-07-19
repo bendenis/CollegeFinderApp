c7621b1619f76f8875840ae3f6401bd3dc1f1fed
bendenis <bdshaff@umich.edu>
2017-07-19 17:56:46 -0400

    New built and data

--- server.r
+++ server.r
@@ -9,27 +9,26 @@ shinyServer(function(input,output) {
         #### PAGE 1
         
         dt1 = reactive({
-                tb = DT %>% filter(Region == input$region, state == input$state,
-                              four_year == input$four_year | two_year == input$two_year,
-                              public_or_private == input$private | public_or_private != input$public,
-                              ugrad_total_pop <= input$ugrad_total_pop, 
-                              coed == input$coed) %>% 
-                        arrange(uni_ranking) %>% as.data.frame()
+                tb = DT %>% filter(Region == input$region, State == input$state,
+                              FourYear == input$four_year | TwoYear == input$two_year,
+                              PublicPrivate == input$private | PublicPrivate != input$public,
+                              UgradTotalPop <= input$ugrad_total_pop, 
+                              COED == input$coed) %>% 
+                        arrange(rank_overall) %>% as.data.frame()
                 
                 if(input$religious == 1){
-                        tb = tb[tb$religious_affiliation == 1,]
+                        tb = tb[tb$ReligiousAffiliation == 1,]
                 }
                 
                 if(input$specialized == 1){
-                        tb = tb[tb$specialized == 1,]
+                        tb = tb[tb$Specialized == 1,]
                 }
                 
-                #tb[sapply(tb$subjects_offered, function(x) str_detect(x, input$major_selecotr)), ]
                 tb
         })
         
         output$list1 = renderDataTable({
-                dt1() %>% select(school_name, city, uni_ranking, selectivity_rank)
+                dt1() %>% select(SchoolName, City, rank_overall, SelectivityRank)
         },
         options = list(pageLength = 20))
         
@@ -45,7 +44,7 @@ shinyServer(function(input,output) {
                         addTiles() %>%
                         addCircleMarkers(lng = ~Longitude, 
                                          lat = ~Latitude, 
-                                         label = ~as.character(dt1()$school_name),
+                                         label = ~as.character(dt1()$SchoolName),
                                          clusterOptions = markerClusterOptions(zIndexOffset = 5),
                                          popup = popup)
         })
@@ -54,45 +53,45 @@ shinyServer(function(input,output) {
         #### PAGE 2 ACADEMIC
         
         dt2 = reactive({
-                dt = dt1() %>% filter(SAT_avg_composite <= input$SAT_avg_composite_up,
-                              SAT_avg_composite >= input$SAT_avg_composite_lw,
-                              SAT_avg_math <= input$SAT_avg_math_up, 
-                              SAT_avg_math >= input$SAT_avg_math_lw,
-                              SAT_avg_reading <= input$SAT_avg_reading_up,
-                              SAT_avg_reading >= input$SAT_avg_reading_lw,
-                              SAT_avg_writing <= input$SAT_avg_writing_up,
-                              SAT_avg_writing >= input$SAT_avg_writing_lw,
-                              ACT_avg_composite <= input$ACT_avg_composite_up,
-                              ACT_avg_composite >= input$ACT_avg_composite_lw,
-                              ACT_avg_math <= input$ACT_avg_math_up,
-                              ACT_avg_math >= input$ACT_avg_math_lw,
-                              ACT_avg_eng <= input$ACT_avg_eng_up,
-                              ACT_avg_eng >= input$ACT_avg_eng_lw,
-                              ACT_avg_writing <= input$ACT_avg_writing_up,
-                              ACT_avg_writing >= input$ACT_avg_writing_lw) %>% 
-                        arrange(uni_ranking)
+                dt = dt1() %>% filter(SATAverageComposite <= input$SAT_avg_composite_up,
+                                      SATAverageComposite >= input$SAT_avg_composite_lw,
+                                      SATAverageMath <= input$SAT_avg_math_up, 
+                                      SATAverageMath >= input$SAT_avg_math_lw,
+                                      SATAverageReading <= input$SAT_avg_reading_up,
+                                      SATAverageReading >= input$SAT_avg_reading_lw,
+                                      SATAverageWriting <= input$SAT_avg_writing_up,
+                                      SATAverageWriting >= input$SAT_avg_writing_lw,
+                                      ACTAverageComposite <= input$ACT_avg_composite_up,
+                                      ACTAverageComposite >= input$ACT_avg_composite_lw,
+                                      ACTAverageMath <= input$ACT_avg_math_up,
+                                      ACTAverageMath >= input$ACT_avg_math_lw,
+                                      ACTAverageEnglish <= input$ACT_avg_eng_up,
+                                      ACTAverageEnglish >= input$ACT_avg_eng_lw,
+                                      ACTAverageWriting <= input$ACT_avg_writing_up,
+                                      ACTAverageWriting >= input$ACT_avg_writing_lw) %>% 
+                        arrange(rank_overall)
                 
                 dt[sapply(dt$subjects_offered, function(x) str_detect(x, input$major_selecotr)), ]
         })
 
         output$list2 = renderDataTable({
-                dt2() %>% select(school_name, state, city,
-                               SAT_range_upper, SAT_range_lower, 
-                               ACT_range_upper, ACT_range_lower,
-                               selectivity_rank)
+                dt2() %>% select(SchoolName, State, City,
+                                 SATRangeUpper, SATRangeLower, 
+                                 ACTRangeUpper, ACTRangeLower,
+                                 SelectivityRank)
         },
         options = list(pageLength = 20))
         
         output$list2sat = renderDataTable({
-                dt2() %>% select(school_name, state, city,
-                                 SAT_range_upper, SAT_range_lower,
-                                 selectivity_rank)
+                dt2() %>% select(SchoolName, State, City,
+                                 SATRangeUpper, SATRangeLower,
+                                 SelectivityRank)
         },
         options = list(pageLength = 20))
         output$list2act = renderDataTable({
-                dt2() %>% select(school_name, state, city,
-                                 ACT_range_upper, ACT_range_lower,
-                                 selectivity_rank)
+                dt2() %>% select(SchoolName, State, City,
+                                 ACTRangeUpper, ACTRangeLower,
+                                 SelectivityRank)
         },
         options = list(pageLength = 20))
         
@@ -108,18 +107,18 @@ shinyServer(function(input,output) {
                         addTiles() %>%
                         addCircleMarkers(lng = ~Longitude, 
                                          lat = ~Latitude, 
-                                         label = ~as.character(dt2()$school_name),
+                                         label = ~as.character(dt2()$SchoolName),
                                          clusterOptions = markerClusterOptions(zIndexOffset = 5),
                                          popup = popup)
         })
         
         output$gpa_density = renderPlot({
-                gpa_dt = DT %>% select(school_name,
+                gpa_dt = DT %>% select(SchoolName,
                                        GPA_3.75_higher, GPA_3.50_3.74, 
                                        GPA_3.25_3.49, GPA_3_3.24, GPA_3_3.24,
                                        GPA_2.50_2.99, GPA_2.0_2.49, GPA_1.0_1.99, GPA_below_1)
                 sp = c(3.875,3.625,3.37,3.12, 2.745, 2.25, 1.495, 0.5)
-                pr = as.numeric(gpa_dt[school_name == input$school_name_gpa,-1]/100)
+                pr = as.numeric(gpa_dt[SchoolName == input$school_name_gpa,-1]/100)
                 dn = sample(sp, size = 10000, prob = pr, replace = T)
                 g = ggplot(data.frame(dn), aes(x = dn)) + 
                         stat_density(bw = 0.2, alpha = 0.5) + 
@@ -130,38 +129,38 @@ shinyServer(function(input,output) {
         #### PAGE 3
         
         dt3 = reactive({
-                tb = dt2() %>% filter(tuition_instate <= input$tuition)
+                tb = dt2() %>% filter(InstateTuition <= input$tuition)
                 
                 if(input$nb_scholarship == 1){
-                        tb = tb[tb$nb_scholarship == 1,]
+                        tb = tb[tb$NeedBasedScholarship == 1,]
                 }
                 if(input$SEOG_scholarship == 1){
-                        tb = tb[tb$SEOG_scholarship == 1,]
+                        tb = tb[tb$SEOGScholarship == 1,]
                 }
                 if(input$state_scholarship == 1){
-                        tb = tb[tb$state_scholarship == 1,]
+                        tb = tb[tb$StateScholarship == 1,]
                 }
                 if(input$college_scholarship == 1){
-                        tb = tb[tb$college_scholarship == 1,]
+                        tb = tb[tb$CollegeScholarship == 1,]
                 }
                 if(input$private_scholarship == 1){
-                        tb = tb[tb$private_scholarship == 1,]
+                        tb = tb[tb$PrivateScholarship == 1,]
                 }
                 if(input$nursing_scholarship == 1){
-                        tb = tb[tb$SEOG_scholarship == 1,]
+                        tb = tb[tb$NursingScholarship == 1,]
                 }
-                if(input$nursing_scholarship == 1){
-                        tb = tb[tb$SEOG_scholarship == 1,]
+                if(input$pell_grant_scholarship == 1){
+                        tb = tb[tb$PellGrantScholarship == 1,]
                 }
                 if(input$united_negro_scholarship == 1){
-                        tb = tb[tb$united_negro_scholarship == 1,]
+                        tb = tb[tb$UnitedNegroScholarship == 1,]
                 }
                 tb
                 
         })
         
         output$list3 = renderDataTable({
-                dt3() %>% select(school_name, city, uni_ranking, tuition_instate, tuition_outstate, selectivity_rank)
+                dt3() %>% select(SchoolName, City, rank_overall, InstateTuition, OutstateTuition, SelectivityRank)
                 
         },
         options = list(pageLength = 20))
@@ -178,7 +177,7 @@ shinyServer(function(input,output) {
                         addTiles() %>%
                         addCircleMarkers(lng = ~Longitude, 
                                          lat = ~Latitude, 
-                                         label = ~as.character(dt3()$school_name),
+                                         label = ~as.character(dt3()$SchoolName),
                                          clusterOptions = markerClusterOptions(zIndexOffset = 5),
                                          popup = popup)
         })
@@ -228,7 +227,7 @@ shinyServer(function(input,output) {
                         addTiles() %>%
                         addCircleMarkers(lng = ~Longitude, 
                                          lat = ~Latitude, 
-                                         label = ~as.character(dt4()$school_name),
+                                         label = ~as.character(dt4()$SchoolName),
                                          clusterOptions = markerClusterOptions(zIndexOffset = 5),
                                          popup = popup)
         })
@@ -236,32 +235,32 @@ shinyServer(function(input,output) {
         athletic_school_name = reactive({input$athletic_school_name})
 
         output$men_club_sports = renderTable({
-                DT %>% filter(school_name == athletic_school_name()) %>%
-                        select(men_club_sports)
+                DT %>% filter(SchoolName == athletic_school_name()) %>%
+                        select(MenClubSports)
         })
 
         output$women_club_sports = renderTable({
-                DT %>% filter(school_name == athletic_school_name()) %>%
-                        select(women_club_sports)
+                DT %>% filter(SchoolName == athletic_school_name()) %>%
+                        select(WomenClubSports)
         })
         
         output$firm_list = renderTable({
-                DT %>% filter(school_name == athletic_school_name()) %>%
-                                                 select(firm_list)
+                DT %>% filter(SchoolName == athletic_school_name()) %>%
+                                                 select(FirmList)
         })
         
         output$grad_list = renderTable({
-                DT %>% filter(school_name == athletic_school_name()) %>%
-                        select(grad_list)
+                DT %>% filter(SchoolName == athletic_school_name()) %>%
+                        select(GradSchoolList)
         })
         
         output$alumni_list = renderTable({
-                DT %>% filter(school_name == athletic_school_name()) %>%
-                        select(alum_list)
+                DT %>% filter(SchoolName == athletic_school_name()) %>%
+                        select(NotableAlumni)
         })
         
         output$diversity_plot = renderPlotly({
-                diversity = DT[DT$school_name == input$athletic_school_name,c(81:83,164:172)]
+                diversity = DT[DT$SchoolName == input$athletic_school_name,c(81:83,164:172)]
                 dv = melt(diversity)
                 dv$tp = c(rep("M/F",2),"Total",rep("ByRace",9))
                 ggplot(dv, aes(x = tp, y = value, fill = variable)) +
@@ -270,16 +269,64 @@ shinyServer(function(input,output) {
         
         output$handicapped_services = renderTable({
                 
-                ix = which(DT$school_name == athletic_school_name())
+                ix = which(DT$SchoolName == athletic_school_name())
                 ls = names(DT[ix,c(55:65)])[which(DT[ix,c(55:65)] == 1)]
                 ls
         })
         
         output$list4 =  renderDataTable({
-                dt4() %>% select(school_name, city, uni_ranking, tuition_instate, tuition_outstate, selectivity_rank)
+                dt4() %>% select(SchoolName, City, rank_overall, InstateTuition, OutstateTuition, SelectivityRank)
                 
         },
         options = list(pageLength = 10))
+        
+        
+        #### PAGE 5 HOUSING
+        
+        dt5 = reactive({
+                tb = dt4()
+                
+                if(input$offered_housing == 1){
+                        tb = tb[tb$HousingOffered == 1,]
+                }
+                if(input$coed_housing == 1){
+                        tb = tb[tb$COEDHousing == 1,]
+                }
+                if(input$men_only_housing == 1){
+                        tb = tb[tb$MenOnlyHousing == 1,]
+                }
+                if(input$women_only_housing == 1){
+                        tb = tb[tb$WomenOnlyHousing == 1,]
+                }
+                if(input$frat_housing == 1){
+                        tb = tb[tb$FratHousing == 1,]
+                }
+                if(input$sorority_housing == 1){
+                        tb = tb[tb$SororityHousing == 1,]
+                }
+                if(input$disability_housing == 1){
+                        tb = tb[tb$DisabilityHousing == 1,]
+                }
+                if(input$international_student_housing == 1){
+                        tb = tb[tb$InternationalStudentHousing == 1,]
+                }
+                if(input$cooperative_housing == 1){
+                        tb = tb[tb$CooperativeHousing == 1,]
+                }
+                if(input$single_student_appartment == 1){
+                        tb = tb[tb$SingleStudentAppartment == 1,]
+                }
+                if(input$married_student_appartment == 1){
+                        tb = tb[tb$MarriedStudentAppartment == 1,]
+                }
+                tb
+                
+        })
+        
+        output$list5 =  renderDataTable({
+                dt5() %>% select(SchoolName, City, rank_overall, InstateTuition, OutstateTuition, SelectivityRank)},
+        options = list(pageLength = 10))
+        
         
 }
 )
--- ui.r
+++ ui.r
@@ -6,7 +6,8 @@ library(ggplot2)
 library(plotly)
 library(data.table)
 library(stringr)
-DT = fread("Data/collegeFinderDataInjected.csv")
+#DT = fread("/Users/bendenisshaffer/Dropbox/myKlovr/CollegeFinder/Data/Interim/collegeFinderDataInjected3.csv")
+DT = fread("Data/D8.csv")
 
 
 shinyUI(navbarPage(title = "College Finder App",theme = shinytheme("united"),
@@ -17,7 +18,7 @@ shinyUI(navbarPage(title = "College Finder App",theme = shinytheme("united"),
                          selectInput(inputId = "region", label = "Region",
                                      choices = unique(DT$Region), selected = "N"),
                          selectInput(inputId = "state", label = "Chose State", 
-                                     choices = unique(DT$state), multiple = T, selected = c("NY")),
+                                     choices = unique(DT$State), multiple = T, selected = c("NY")),
                          h1("School Type"),
                          sliderInput(inputId = "ugrad_total_pop", label = "School Size", 
                                      min = 0, max = 70000, step = 20, value = 40000),
@@ -106,13 +107,13 @@ shinyUI(navbarPage(title = "College Finder App",theme = shinytheme("united"),
                                     dataTableOutput("list2act")
                             )
                             
-                            )#,
+                            ),
                    
                    
-                   # tabPanel("GPA Distributions",
-                   #          selectInput(inputId = "school_name_gpa", label = "Type School Name", choices = unique(DT$school_name),
-                   #                      selected = "University of Michigan -- Ann Arbor"),
-                   #          plotOutput("gpa_density"))
+                   tabPanel("GPA Distributions",
+                            selectInput(inputId = "school_name_gpa", label = "Type School Name", choices = unique(DT$SchoolName),
+                                        selected = "University of Michigan -- Ann Arbor"),
+                            plotOutput("gpa_density"))
                    
         ),
         
@@ -138,59 +139,84 @@ shinyUI(navbarPage(title = "College Finder App",theme = shinytheme("united"),
                         )
                  
                  ),
-
-        tabPanel(title = "University Life",
-                sidebarPanel(
-                        h1("Disabilities"),
-                        checkboxInput(inputId = "handicapped_housing", label = "Handicapped Housing"),
-                        checkboxInput(inputId = "handicapped_notetaking", label = "Notetaking for Handicapped Students"),
-                        checkboxInput(inputId = "handicapped_taprecorder", label = "Taperecoding for Handicapped Students"),
-                        checkboxInput(inputId = "handicapped_tutor", label = "Tutoring for Handicapped Students"),
-                        checkboxInput(inputId = "handicapped_reader", label = "Reader for Handicapped Students"),
-                        checkboxInput(inputId = "handicapped_interpeter", label = "Interpreter for Handicapped Students"),
-                        checkboxInput(inputId = "handicapped_transport", label = "Transportation for Handicapped Students"),
-                        checkboxInput(inputId = "handicapped_taperecorder", label = "Taperecoding for Handicapped Students"),
-                        checkboxInput(inputId = "handicapped_equipment", label = "Equipment for Handicapped Students"),
-                        checkboxInput(inputId = "handicapped_braille", label = "Braille Code on Campus"),
-                        checkboxInput(inputId = "handicapped_talkbook", label = "Talkbook for Handicapped Students")
-                ),
-                 
-                mainPanel(
-                         dataTableOutput("list4")
-                ),
-                 
-                leafletOutput("college_map4"),
-                 
-                selectInput(inputId = "athletic_school_name", 
-                            selected = "University of Michigan -- Ann Arbor", 
-                            label = "Type School Name", 
-                            choices = DT$school_name),
-                         
-                fluidRow(
-                        h1("Club Sports"),
-                        column(4,tableOutput("men_club_sports")),
-                        column(4,tableOutput("women_club_sports")),
-                        h1("Notable Alumni"),
-                        column(4,tableOutput("alumni_list"))
-                        ),
-                         
-                fluidRow(
-                        h1("After Graduating"),
-                        column(6, tableOutput("firm_list")),
-                        column(6, tableOutput("grad_list"))
-                         ),
-                      
-                fluidRow(
-                        h1("Disability Services"),
-                        column(6, tableOutput("handicapped_services"))
-                ),
+        
+        navbarMenu(title = "Univesity Life",
                    
-                fluidRow(
-                        h1("Diversity"),
-                        column(10,plotlyOutput("diversity_plot"))
-                         )
-                         
-        )
+                   tabPanel(title = "Campus Accessibility",
+                            sidebarPanel(
+                                    h1("Disabilities"),
+                                    checkboxInput(inputId = "handicapped_housing", label = "Handicapped Housing"),
+                                    checkboxInput(inputId = "handicapped_notetaking", label = "Notetaking for Handicapped Students"),
+                                    checkboxInput(inputId = "handicapped_taprecorder", label = "Taperecoding for Handicapped Students"),
+                                    checkboxInput(inputId = "handicapped_tutor", label = "Tutoring for Handicapped Students"),
+                                    checkboxInput(inputId = "handicapped_reader", label = "Reader for Handicapped Students"),
+                                    checkboxInput(inputId = "handicapped_interpeter", label = "Interpreter for Handicapped Students"),
+                                    checkboxInput(inputId = "handicapped_transport", label = "Transportation for Handicapped Students"),
+                                    checkboxInput(inputId = "handicapped_taperecorder", label = "Taperecoding for Handicapped Students"),
+                                    checkboxInput(inputId = "handicapped_equipment", label = "Equipment for Handicapped Students"),
+                                    checkboxInput(inputId = "handicapped_braille", label = "Braille Code on Campus"),
+                                    checkboxInput(inputId = "handicapped_talkbook", label = "Talkbook for Handicapped Students")
+                            ),
+                            
+                            mainPanel(
+                                    dataTableOutput("list4")
+                            ),
+                            
+                            leafletOutput("college_map4"),
+                            
+                            selectInput(inputId = "athletic_school_name", 
+                                        selected = "University of Michigan -- Ann Arbor", 
+                                        label = "Type School Name", 
+                                        choices = DT$SchoolName),
+                            
+                            fluidRow(
+                                    h1("Club Sports"),
+                                    column(4,tableOutput("men_club_sports")),
+                                    column(4,tableOutput("women_club_sports")),
+                                    h1("Notable Alumni"),
+                                    column(4,tableOutput("alumni_list"))
+                            ),
+                            
+                            fluidRow(
+                                    h1("After Graduating"),
+                                    column(6, tableOutput("firm_list")),
+                                    column(6, tableOutput("grad_list"))
+                            ),
+                            
+                            fluidRow(
+                                    h1("Disability Services"),
+                                    column(6, tableOutput("handicapped_services"))
+                            ),
+                            
+                            fluidRow(
+                                    h1("Diversity"),
+                                    column(10,plotlyOutput("diversity_plot"))
+                            )
+                            
+                        ),
+                   
+                   tabPanel(title = "Housing",
+                           sidebarPanel(
+                                   h1("Housing"),
+                                   checkboxInput(inputId = "offered_housing", label = "Housing is Offered"),
+                                   checkboxInput(inputId = "coed_housing", label = "COED Housing"),
+                                   checkboxInput(inputId = "women_only_housing", label = "Women Only Housing"),
+                                   checkboxInput(inputId = "men_only_housing", label = "Men Only Housing"),
+                                   checkboxInput(inputId = "sorority_housing", label = "Sorority Housing"),
+                                   checkboxInput(inputId = "frat_housing", label = "Fraternity Housing"),
+                                   checkboxInput(inputId = "single_student_appartment", label = "Single Student Appartment"),
+                                   checkboxInput(inputId = "married_student_appartment", label = "Married Student Appartment"),
+                                   checkboxInput(inputId = "disability_housing", label = "Disability Housing"),
+                                   checkboxInput(inputId = "international_student_housing", label = "International Student Housing"),
+                                   checkboxInput(inputId = "cooperative_housing", label = "Cooperative Housing")
+                           ),
+                           
+                           mainPanel(
+                                   dataTableOutput("list5")
+                           )
+                           
+                        )
+                   )
         
 )
 )
